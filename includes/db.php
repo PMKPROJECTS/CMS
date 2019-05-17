a9672288593fdf87e531dadaffbc07e826146f74
PROJECTS <PROJECTS@Vinays-MacBook-Pro.local>
2019-05-17 09:32:43 +0530

    till_96_videos

---
+++ includes/db.php
@@ -0,0 +1,41 @@
+<?php 
+//1st way
+/*
+$connection = mysqli_connect("localhost","root","","cms");
+
+if($connection)
+{
+    echo "we are connected";
+}
+*/
+//2nd way
+/*
+$db["hostname"] = "localhost";
+$db["username"] = "root";
+$db["password"] = "";
+$db["dbname"] = "cms";
+
+foreach($db as $key => $value){
+    define(strtoupper($key), $value);
+}
+
+$connection = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DBNAME);
+
+if($connection)
+{
+    echo "we are connected";
+}
+*/
+
+//3rd way
+$dbserver = "localhost";
+$dbuser = "root";
+$dbpassword = "";
+$dbname = "cms";
+
+$connection = mysqli_connect($dbserver,$dbuser,$dbpassword,$dbname);
+if(!$connection)
+{
+    echo "not connected";
+}
+?>
